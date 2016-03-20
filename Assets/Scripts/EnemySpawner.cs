62b3c1f0cfe2d1b8f1cd437e743bce147cd5a0ed
Refactula <refactula@gmail.com>
2016-03-20 11:18:45 +0200

    Add Enemies spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public int SpawnCount = 3;
+
+	void Start () {
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		int remaining = SpawnCount;
+		for (int i = 0; i < enemies.Length; i++) {
+			double chance = 1f * remaining / (enemies.Length - i);
+			if (Random.value <= chance) {
+				remaining--;
+			} else {
+				enemies [i].SetActive (false);
+			}
+		}
+	}
+
+}
